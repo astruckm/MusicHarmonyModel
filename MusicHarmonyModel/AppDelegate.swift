df3de25c25f76c048e8694de3646daac4c4d14e3
Andrew Struck-Marcell <astruckm@yahoo.com>
2020-12-31 15:33:56 +0900

    Initial Commit

---
+++ MusicHarmonyModel/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MusicHarmonyModel
+//
+//  Created by Andrew Struck-Marcell on 12/31/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
